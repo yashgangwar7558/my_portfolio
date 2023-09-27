20b45cbf8a7ab6f6656d8ed225aa600c848396e3
Yash Gangwar <gangwaryash06@gmail.com>
2023-09-27 23:41:25 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
       {['home', 'about', 'work', 'skills', 'contact', 'resume', 'blogs'].map((item) => (
           <li key={`link-${item}`} className="app__flex p-text">
             <div />
-            <a href={item == 'resume' || item == 'blogs' ? (item == 'resume' ? `https://drive.google.com/file/d/1cPmD0gVHfUGo8l3rf04XqjKVulMeroOe/view?usp=share_link` : `https://yashgangwar7558.hashnode.dev/`) : (`#${item}`)}>{item}</a>
+            <a href={item == 'resume' || item == 'blogs' ? (item == 'resume' ? `https://drive.google.com/file/u/2/d/1FPXJSrCTB_14FhvuS4ZY0DCvBNHIr0VW/view?usp=drivesdk` : `https://yashgangwar7558.hashnode.dev/`) : (`#${item}`)}>{item}</a>
           </li>
         ))}
       </ul>
